2fbdecd130f465ca650d50de9de9d3f4b26f4ab7
Roche Stapelberg <stapelberg.roche@gmail.com>
2021-10-06 15:22:33 +0200

    Add project files.

---
+++ PRG282Project/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PRG282Project.PresentationLayer;
+
+namespace PRG282Project
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmLogin());
+        }
+
+        //TODO - FileHandler
+        //TODO - DataHandler
+        //TODO - Forms
+        //TODO - DB Access
+    }
+}
