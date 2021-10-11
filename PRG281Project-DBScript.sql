ee3d229cbc38be5df0b5e0c077421c90b618f553
Roche Stapelberg <stapelberg.roche@gmail.com>
2021-10-11 21:36:36 +0200

    Fixed Bugs in Database Script

--- PRG281Project-DBScript.sql
+++ PRG281Project-DBScript.sql
@@ -1,10 +1,8 @@
-﻿DROP DATABASE BelgiumStudents
-GO
-CREATE DATABASE BelgiumStudents
+﻿CREATE DATABASE BelgiumStudents
 GO
 USE BelgiumStudents
 GO
-CREATE TABLE Students
+CREATE TABLE Student
 (
 	StudentNumber int,
 	StudentName VARCHAR(50),
@@ -28,37 +26,37 @@ CREATE TABLE Module
 	ModuleLink VARCHAR(100),
 );
 GO
-INSERT INTO Module VALUES ('PRG281','Programming 281','Indepth module on C#','https://www.youtube.com/watch?v=GhQdlIFylQ8&t=35s');
-INSERT INTO Module VALUES ('PRG282','Programming with Database','C# and SQL sever manager combined','https://www.youtube.com/watch?v=GVV-LUcmCOE');
-INSERT INTO Module VALUES ('MAT281','Math','The unsolvable math problem','https://www.youtube.com/watch?v=094y1Z2wpJg&t=480s');
-INSERT INTO Module VALUES ('DBD281','Database','SQL Sever','https://www.youtube.com/watch?v=WttoAlS__8k');
-INSERT INTO Module VALUES ('STA281','Statistics','Intro to stats','https://www.youtube.com/watch?v=GUQJ7zMoSCM&t=3s');
+INSERT INTO Module VALUES ('PRG281', 1, 'Programming 281','Indepth module on C#','https://www.youtube.com/watch?v=GhQdlIFylQ8&t=35s');
+INSERT INTO Module VALUES ('PRG282', 2, 'Programming with Database','C# and SQL sever manager combined','https://www.youtube.com/watch?v=GVV-LUcmCOE');
+INSERT INTO Module VALUES ('MAT281', 3, 'Math','The unsolvable math problem','https://www.youtube.com/watch?v=094y1Z2wpJg&t=480s');
+INSERT INTO Module VALUES ('DBD281', 4, 'Database','SQL Sever','https://www.youtube.com/watch?v=WttoAlS__8k');
+INSERT INTO Module VALUES ('STA281', 5,'Statistics','Intro to stats','https://www.youtube.com/watch?v=GUQJ7zMoSCM&t=3s');
 GO
-INSERT INTO Students VALUES (1, 'Ryan', 'Mc Callum', '18-01-2001','Male','084 487 0795', '214 North Street','');
-INSERT INTO Students VALUES (2, 'Roche', 'Stapelberg', '07-07-1999','Male','083 140 7817', '546 Tait St','');
-INSERT INTO Students VALUES (3, 'Herman', 'Potgieter', '08-02-2001','Male','084 621 0817', '2086 Broad Rd','');
-INSERT INTO Students VALUES (4, 'Liam', 'Smith', '20-02-2001','Male','082 883 9562', '2416 Wattle St','');
-INSERT INTO Students VALUES (5, 'Emma', 'Johnson', '10-07-2002','Female','085 458 6960', '798 Main Rd','');
-INSERT INTO Students VALUES (6, 'Noah', 'Brown', '14-08-1998','Male','084 207 6462', '1557 Gemsbok St','');
-INSERT INTO Students VALUES (7, 'Ava', 'Jones', '16-01-1996','Female','083 914 5918', '1741 Gleemoor Rd','');
-INSERT INTO Students VALUES (8, 'Oliver', 'Garcia', '22-04-2001','Male','083 629 2913', '930 Fox St','');
-INSERT INTO Students VALUES (9, 'Sophia', 'Miller', '08-05-2001','Female','083 469 3418', '1324 Mosman Rd','');
-INSERT INTO Students VALUES (10, 'Amelia', 'Davis', '11-11-1998','Female','085 951 3990', '327 Roger St','');
-INSERT INTO Students VALUES (11, 'Isabella', 'Rodriguez', '05-01-2000','Female','085 643 8620', '895 Hoog St','');
-INSERT INTO Students VALUES (12, 'Mia', 'Thomas', '26-08-2001','Female','085 355 7216', '900 Kort St','');
-INSERT INTO Students VALUES (13, 'James', 'Jackson', '27-04-2001','Male','084 180 5595', '849 Fox St','');
-INSERT INTO Students VALUES (14, 'William', 'Taylor', '28-04-1998','Male','083 913 4967', '463 Bath Rd','');
-INSERT INTO Students VALUES (15, 'Simba', 'Zengeni', '06-06-1989','Male','085 431 0333', '538 Uitsig St','');
-INSERT INTO Students VALUES (16, 'Tendi', 'Mkwaira', '17-04-1988','Male','083 674 1739', '1568 Thomas St','');
-INSERT INTO Students VALUES (17, 'Valdez', 'Pretourius', '29-09-1992','Male','082 754 2679', '2209 Bath Rd','');
-INSERT INTO Students VALUES (18, 'Lucas', 'Thompson', '26-12-2001','Male','083 522 4432', '2049 Mosman Rd','');
-INSERT INTO Students VALUES (19, 'Deborah', 'Oluwadele', '24-07-1991','Female','082 476 6993', '1881 St. John Street','');
-INSERT INTO Students VALUES (20, 'Alex', 'Harris', '02-02-2001','Male','082 506 6176', '1545 Doreen St','');
+INSERT INTO Student VALUES (1, 'Ryan', 'Mc Callum', '18-01-2001','Male','084 487 0795', '214 North Street', NULL);
+INSERT INTO Student VALUES (2, 'Roche', 'Stapelberg', '07-07-1999','Male','083 140 7817', '546 Tait St', NULL);
+INSERT INTO Student VALUES (3, 'Herman', 'Potgieter', '08-02-2001','Male','084 621 0817', '2086 Broad Rd', NULL);
+INSERT INTO Student VALUES (4, 'Liam', 'Smith', '20-02-2001','Male','082 883 9562', '2416 Wattle St', NULL);
+INSERT INTO Student VALUES (5, 'Emma', 'Johnson', '10-07-2002','Female','085 458 6960', '798 Main Rd', NULL);
+INSERT INTO Student VALUES (6, 'Noah', 'Brown', '14-08-1998','Male','084 207 6462', '1557 Gemsbok St', NULL);
+INSERT INTO Student VALUES (7, 'Ava', 'Jones', '16-01-1996','Female','083 914 5918', '1741 Gleemoor Rd', NULL);
+INSERT INTO Student VALUES (8, 'Oliver', 'Garcia', '22-04-2001','Male','083 629 2913', '930 Fox St', NULL);
+INSERT INTO Student VALUES (9, 'Sophia', 'Miller', '08-05-2001','Female','083 469 3418', '1324 Mosman Rd', NULL);
+INSERT INTO Student VALUES (10, 'Amelia', 'Davis', '11-11-1998','Female','085 951 3990', '327 Roger St', NULL);
+INSERT INTO Student VALUES (11, 'Isabella', 'Rodriguez', '05-01-2000','Female','085 643 8620', '895 Hoog St', NULL);
+INSERT INTO Student VALUES (12, 'Mia', 'Thomas', '26-08-2001','Female','085 355 7216', '900 Kort St', NULL);
+INSERT INTO Student VALUES (13, 'James', 'Jackson', '27-04-2001','Male','084 180 5595', '849 Fox St', NULL);
+INSERT INTO Student VALUES (14, 'William', 'Taylor', '28-04-1998','Male','083 913 4967', '463 Bath Rd', NULL);
+INSERT INTO Student VALUES (15, 'Simba', 'Zengeni', '06-06-1989','Male','085 431 0333', '538 Uitsig St', NULL);
+INSERT INTO Student VALUES (16, 'Tendi', 'Mkwaira', '17-04-1988','Male','083 674 1739', '1568 Thomas St', NULL);
+INSERT INTO Student VALUES (17, 'Valdez', 'Pretourius', '29-09-1992','Male','082 754 2679', '2209 Bath Rd', NULL);
+INSERT INTO Student VALUES (18, 'Lucas', 'Thompson', '26-12-2001','Male','083 522 4432', '2049 Mosman Rd', NULL);
+INSERT INTO Student VALUES (19, 'Deborah', 'Oluwadele', '24-07-1991','Female','082 476 6993', '1881 St. John Street', NULL);
+INSERT INTO Student VALUES (20, 'Alex', 'Harris', '02-02-2001','Male','082 506 6176', '1545 Doreen St', NULL);
 GO
 CREATE PROCEDURE spGetStudents
 AS
 BEGIN
-	SELECT * FROM Students
+	SELECT * FROM Student
 END
 GO
 CREATE PROCEDURE spAddStudent
@@ -73,8 +71,8 @@ CREATE PROCEDURE spAddStudent
 )
 AS
 BEGIN
-	INSERT INTO Students
-	VALUES (@StudentNumber, @StudentName, @StudentSurname, @DateofBirth, @Gender, @StudentPhoneNumber, @StudentAddress)
+	INSERT INTO Student
+	VALUES (@StudentNumber, @StudentName, @StudentSurname, @DateofBirth, @Gender, @StudentPhoneNumber, @StudentAddress, NULL)
 END
 GO
 CREATE PROCEDURE spUpdateStudent
@@ -89,14 +87,15 @@ CREATE PROCEDURE spUpdateStudent
 )
 AS
 BEGIN
-	UPDATE Students
+	UPDATE Student
 	SET StudentNumber = @StudentNumber,
 		StudentName = @StudentName, 
 		StudentSurname = @StudentSurname, 
 		DateOfBirth = @DateofBirth, 
 		Gender = @Gender, 
 		StudentPhoneNumber = @StudentPhoneNumber, 
-		StudentAddress = @StudentAddress
+		StudentAddress = @StudentAddress,
+		StudentPhoto = NULL
 		WHERE StudentNumber = @StudentNumber
 END
 GO
--- PRG282Project/Program.cs
+++ PRG282Project/Program.cs
@@ -17,7 +17,7 @@ namespace PRG282Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new frmMain());
         }
 
         //TODO - FileHandler
