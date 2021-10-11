13c907e55e55d709b81d241d94e7d9f63e4965c4
Herman Potgieter <hermanpotgieterjnr@gmail.com>
2021-10-11 10:25:00 +0200

    Made LogIn Page(not Final)

--- PRG282Project/PresentationLayer/frmLogin.cs
+++ PRG282Project/PresentationLayer/frmLogin.cs
@@ -16,5 +16,20 @@ namespace PRG282Project.PresentationLayer
         {
             InitializeComponent();
         }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblLine1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblHeading_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
